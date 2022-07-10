3e1e7e06af87a9f716f96d693b35022aeee422bf
Regulus735 <regulus735@gmail.com>
2022-07-10 18:00:59 +0200

    change redirect URI

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Login.sass';
 
 const URL =
-   'https://accounts.spotify.com/authorize?client_id=ca558bcd1da840a08d33b77e91180588&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
+   'https://accounts.spotify.com/authorize?client_id=ca558bcd1da840a08d33b77e91180588&response_type=code&redirect_uri=https://regulus735.github.io/spotify-player/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
 
 const Login: React.FC = () => {
    return (
