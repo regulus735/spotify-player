d67b730121ac04730870fbeb96b4a85d887afd79
Regulus735 <regulus735@gmail.com>
2022-07-14 13:51:47 +0200

    add table header

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,27 +5,46 @@ import Track from './Track';
 import { useAppSelector } from '../store/hooks';
 import './Main.sass';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
+
 const Main: React.FC = () => {
    const playlistTracks = useAppSelector((state) => state.chosenPlaylist);
 
-   const tracksContainer = playlistTracks.tracks.map((item) => (
+   const tracksContainer = playlistTracks.tracks.map((item, index) => (
       <Track
          key={item.uri}
          name={item.name}
          album={item.album}
          artists={item.artists}
          uri={item.uri}
+         duration_ms={item.duration_ms}
+         index={index + 1}
       />
    ));
 
    return (
       <main className="main">
          <h1>{playlistTracks.playlistName}</h1>
-         <div className="tracts-list">
-            {playlistTracks.isEveryTrackLoaded && tracksContainer}
-            {!playlistTracks.isEveryTrackLoaded &&
-               playlistTracks.playlistId && <Loading />}
+         {!playlistTracks.isEveryTrackLoaded && playlistTracks.playlistId && (
+            <Loading />
+         )}
+
+         <div className="tracks-list">
+            {playlistTracks.isEveryTrackLoaded && (
+               <>
+                  <header>
+                     <span className="index">#</span>
+                     <span className="title">Title</span>
+                     <span className="duration">
+                        <FontAwesomeIcon icon={faClock} />
+                     </span>
+                  </header>
+                  {tracksContainer}
+               </>
+            )}
          </div>
+
          <Player />
       </main>
    );
